4539ab00229d6642576e85be445f95ac010a152e
cherbobs <124584178+cherbobs@users.noreply.github.com>
2023-11-24 15:25:39 +0100

    Add files via upload

---
+++ src/script_followingArtists.js
@@ -0,0 +1,67 @@
+import { getAccessToken } from "./config.js";
+
+
+const clientId = "33c2366304a1438db1ff72675608d71e";
+const accessToken = localStorage.getItem("accessToken");
+ const newAccessToken = await getAccessToken(clientId, accessToken, null, "followingArtist.html");
+// Stockez le nouveau token d'accès si nécessaire
+localStorage.setItem("accessToken", newAccessToken);
+
+
+
+if (!accessToken && !refreshToken) {
+    document.location.href="./index.html";
+} else {
+    const followingArtists = await fetchFollowingArtist(accessToken);
+    followingArtists.artists.items.forEach(displayFollowingArtists);
+    console.log(followingArtists);
+    
+}
+
+
+
+async function fetchFollowingArtist(token) {
+    const result = await fetch(`https://api.spotify.com/v1/me/following?type=artist`, {
+        method: "GET", headers: { Authorization: `Bearer ${token}` }
+    });
+    return await result.json();
+    
+} 
+
+
+function displayFollowingArtists(artist) {
+    const artistId = artist.id;
+    const artistName = artist.name;
+    const artistImage = new Image(200, 200);
+    artistImage.src = artist.images[0].url;
+    
+    artistImage.onload = function () {
+        // Créer des éléments pour chaque artiste
+        const artisteDiv = document.createElement("div");
+        artisteDiv.id = artistId;
+    
+        const imageDiv = document.createElement("div");
+        imageDiv.style.backgroundImage = `url(${artistImage.src})`;
+        imageDiv.className = "image";
+    
+        const nameParagraph = document.createElement("p");
+        nameParagraph.textContent = artistName;
+    
+        // Ajouter les éléments à la div "artistes"
+        artisteDiv.appendChild(imageDiv);
+        artisteDiv.appendChild(nameParagraph);
+    
+        document.getElementById("artistes").appendChild(artisteDiv);
+
+        // Ajouter un gestionnaire d'événements de clic pour rediriger vers artiste.html
+        artisteDiv.addEventListener("click", function () {
+            window.location.href = `artist.html?id=${artistId}`;
+        });
+        
+
+        // Gestion des erreurs pour l'image
+        artistImage.onerror = function () {
+            console.error("Erreur de chargement de l'image pour l'artiste", artistName);
+        };
+    };
+}
